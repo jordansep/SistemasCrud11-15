0ca5101ad0c4843b0b5cfd8c722777fb06ac288d
jordansep <jordanmonier4@gmail.com>
2025-06-30 13:12:47 -0300

    Implementado Sistema Loggin en actividadOnce

--- Ejercicios/Ejercicio11/ejercicioOnce.cs
+++ Ejercicios/Ejercicio11/ejercicioOnce.cs
@@ -18,8 +18,9 @@ namespace SistemasCrud10_15.Ejercicios.Ejercicio11
             b.ShowLines(Ruta.pathFile);
             Formato.Separador();
             Formato.EscribirLinea("1) Registrar usuario nuevo");
-            Formato.EscribirLinea("2) Modificar usuario");
-            Formato.EscribirLinea("3) Eliminar usuario por ID");
+            Formato.EscribirLinea("2) Loguearse.");
+            Formato.EscribirLinea("3) Modificar usuario");
+            Formato.EscribirLinea("4) Eliminar usuario por ID");
             Formato.Separador();
             int opciones = Validaciones.ValidarEntero(1,3,"Ingrese una opcion");
             Formato.Separador();
@@ -27,8 +28,9 @@ namespace SistemasCrud10_15.Ejercicios.Ejercicio11
             switch (opciones)
             {
                 case 1: b.registrarUsuario(); break;
-                case 2: b.ModLine(Validaciones.ValidarEntero(1, max, "Que usuario desea modificar?")); break;
-                case 3: b.DeleteID(Validaciones.ValidarEntero(1, max, "Que usuario desea eliminar?")); break;
+                case 2: b.Loggin(); break;
+                case 3: b.ModLine(Validaciones.ValidarEntero(1, max, "Que usuario desea modificar?")); break;
+                case 4: b.DeleteID(Validaciones.ValidarEntero(1, max, "Que usuario desea eliminar?")); break;
             }
         }
     }
--- Modulos/archiveManHerency/registroUsuarios.cs
+++ Modulos/archiveManHerency/registroUsuarios.cs
@@ -13,21 +13,51 @@ namespace SistemasCrud10_15.Modulos.archiveManHerency
         private static string nombre;
         private static string email;
         private static int edad;
+        private static string contraseña;
         public registroUsuarios()
         {
             nombre = string.Empty;
             email = string.Empty;
             edad = 0;
+            contraseña = "1";
         }
         public void registrarUsuario()
+
         {
             id = UniqueID();
             nombre = Validaciones.ValidarTexto("Ingrese el nombre de usuario");
             email = Validaciones.ValidarTexto("Ingrese el Correo Electronico");
             edad = Validaciones.ValidarEntero(1,100,"Ingrese la edad del usuario");
-            string registrar = string.Join(",", id, nombre, email, edad);
+            contraseña = Validaciones.ValidarTexto("Ingrese la contraseña del usuario");
+
+            string registrar = string.Join(",", id, nombre, email, edad, contraseña);
             listLines.Add(registrar);
             File.WriteAllLines(pathFile, listLines.ToArray());
+        }
+        public void Loggin()
+        {
+            Formato.EscribirLinea("Ingrese el nombre de usuario o email");
+            nombre = Validaciones.ValidarTexto("Ingrese el nombre de usuario o email");
+            contraseña = Validaciones.ValidarTexto("Ingrese la contraseña");
+            lineas = getLines(pathFile);
+            for (int i = 0; i < lineas.Length; i++)
+            {
+                string[] datos = lineas[i].Split(',');
+                if (datos[1] == nombre && datos[4] == contraseña || datos[2] == nombre && datos[4] == contraseña)
+                {
+                    Formato.EscribirLinea("Logueado correctamente");
+                    Formato.EscribirLinea($"Bienvenido {datos[1]}");
+                    int opciones = Validaciones.ValidarEntero(1, 3, "Que desea hacer? \n1) Modificar datos \n2) Eliminar usuario \n3) Salir");
+                    switch (opciones)
+                    {
+                        case 1: ModLine(Validaciones.ValidarEntero(1, datos.Length, "Que dato desea modificar?")); break;
+                        case 2: DeleteID(int.Parse(datos[0])); break;
+                        case 3: return;
+                    }
+
+                    return;
+                }
+            }
         }
         
     }
